46d8eaee58e4c141fb33a7d061b3c2a236376a69
Agustin <agustinxmtz@gmail.com>
2021-06-09 17:44:27 -0500

    Added Game over after invaders reach the bottom

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const grid = document.querySelector('.grid')
+const resultsDisplay = document.querySelector('.results')
 let currentShooterIndex = 202
 let width = 15
+let direction = 1
+let invadersId
+let goingRight = true
 
 for (let i = 0; i < 225; i++ ) {
     const square = document.createElement('div')
@@ -23,8 +27,14 @@ function draw() {
 
 draw()
 
+function remove() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+        squares[alienInvaders[i]].classList.remove('invader')
+    }
+}
 
-squares[currentShooterIndex].classList.add('shooter')
+
+// squares[currentShooterIndex].classList.add('shooter')
 
 function moveShooter(e) {
     squares[currentShooterIndex].classList.remove('shooter')
@@ -39,3 +49,50 @@ function moveShooter(e) {
     squares[currentShooterIndex].classList.add('shooter')
 }
 document.addEventListener('keydown', moveShooter)
+
+function moveInvaders() {
+    const leftEdge = alienInvaders[0] % width === 0
+    const rightEdge = alienInvaders[alienInvaders.length -1] % width === width -1
+    remove()
+
+    if ( rightEdge && goingRight ) {
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += width +1
+            direction = -1
+            goingRight = false
+        }
+    }
+
+    if (leftEdge && !goingRight) {
+        for (let i = 0; i < alienInvaders.length; i++) {
+            alienInvaders[i] += width -1
+            direction = 1
+            goingRight = true
+        }
+    }
+
+    for ( let i = 0; i < alienInvaders.length; i++) {
+        alienInvaders[i] += direction
+    }
+
+    draw()
+
+    if (squre[currentShooterIndex].classList.contains('invader', 'shooter')) {
+        resultsDisplay.innerHTML = 'GAME OVER'
+        clearInterval(invadersId)
+    }
+
+    if (squre[currentShooterIndex].classList.contains('invader', 'shooter')) {
+        resultsDisplay.innerHTML = 'GAME OVER'
+        clearInterval(invadersId)
+    }
+
+    for (let i = 0; i < alienInvaders.length; i++) {
+        if(alienInvaders[i] > (squares.length)) {
+            resultsDisplay.innerHTML = 'GAME OVER'
+        }
+    }
+
+}
+
+invadersId = setInterval(moveInvaders, 500)
